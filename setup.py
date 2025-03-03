473c3d41285e907cc5045c2cc837d5b87f9659b3
alexiisjbaptiste <alexis.jeanbaptiste@icloud.com>
2025-03-03 23:39:54 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="easyml",
-    version="0.1.0",
-    description="A one-liner ML training and deployment library",
+    name="easyml",  # Package name (users will install with pip install easyml)
+    version="0.1.0",  # Update version as needed
+    description="A lightweight, one-liner ML training and deployment library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Your Name",
     author_email="your.email@example.com",
-    url="https://github.com/alexiisjbaptiste/easyml",
+    url="https://github.com/YOUR_GITHUB_USERNAME/easyml",  # GitHub repo
     packages=find_packages(),
     install_requires=[
         "pandas",
@@ -21,5 +23,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires=">=3.7",
 )
